ffbdc82db596bf4a5c37aef02a57047f12067e72
ravindrasinghh <rvisingh1221@gmail.com>
2019-12-06 18:01:32 +0530

    Added IAM Role to EC2

--- Elasticsearch.tf
+++ Elasticsearch.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "elasticsearch_instance" {
   ami           = "ami-0927ed83617754711"
   instance_type = "${var.aws_instance_type}"
- # security_groups = ["${aws_security_group.elasticsearch_cluster_sg.name}"]
+  security_groups = ["${aws_security_group.elasticsearch_cluster_sg.name}"]
   #iam_instance_profile = "${aws_iam_instance_profile.elasticsearch_profile.name}"
   #user_data = "${file("elasticsearch-node-setup.sh")}"
   count = "1"
