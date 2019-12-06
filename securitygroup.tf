c51d0485c2ab0ba5f411e5de8c3cd7c36d594b76
ravindrasinghh <rvisingh1221@gmail.com>
2019-12-06 17:55:58 +0530

    Added Security Group

---
+++ securitygroup.tf
@@ -0,0 +1,33 @@
+# Security Group - allow SSH and TCP on port 9200 and 9300 (Elasticsearch API endpoint)
+resource "aws_security_group" "elasticsearch_cluster_sg" {
+  name        = "elasticsearch_cluster_sg"
+  description = "Elasticsearch security group"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 9200
+    to_port     = 9200
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 9300
+    to_port     = 9300
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
