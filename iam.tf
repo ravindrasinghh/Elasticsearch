6757e4abd113581a773962c2d13f8e7253debe0e
ravindrasinghh <rvisingh1221@gmail.com>
2019-12-07 14:42:40 +0530

    Added IAM Policy

--- iam.tf
+++ iam.tf
@@ -1,43 +1,63 @@
+resource "aws_iam_instance_profile" "elasticsearchrole" {
+  name = "elasticsearchrole"
+  role = "${aws_iam_role.elasticsearchrole.name}"
+}
 resource "aws_iam_role" "elasticsearchrole" {
-  name = "elasticsearch-role"
+  name = "test-role"
 
-  assume_role_policy = <<EOF
-    {
-      "Version": "2012-10-17",
-      "Statement": [
-        {
-          "Action": "sts:AssumeRole",
-          "Principal": {
-            "Service": "ec2.amazonaws.com"
-          },
-          "Effect": "Allow",
-          "Sid": ""
-        }
-      ]
-    }
-EOF
-}
 
-resource "aws_iam_policy" "policy" {
-  name        = "elasticsearch-policy"
-
-  policy = <<EOF
+  assume_role_policy = <<POLICY
 {
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "ec2:Describe*"
-      ],
-      "Effect": "Allow",
-      "Resource": "*"
-    }
-  ]
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "sts:AssumeRole",
+            "Principal": {
+                "Service": "ec2.amazonaws.com"
+            }
+        }
+    ]
 }
-EOF
+POLICY
 }
 
-resource "aws_iam_role_policy_attachment" "test-attach" {
-  role       = "${aws_iam_role.role.name}"
-  policy_arn = "${aws_iam_policy.policy.arn}"
+resource "aws_iam_role_policy" "role_policy" {
+  role = "${aws_iam_role.elasticsearchrole.id}"
+  name = "kong-role-policy"
+
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "s3:*",
+            "Effect": "Allow",
+            "Resource": "*"
+        },
+        {
+            "Action": "ssm:GetParameter",
+            "Effect": "Allow",
+            "Resource": "*"
+        },
+        {
+          "Action": "rds:*",
+            "Effect": "Allow",
+            "Resource": "*"
+        },
+        {
+          "Action": "kms:*",
+            "Effect": "Allow",
+            "Resource": "*"
+        }
+    ]
 }
+POLICY
+}
+
+
+resource "aws_iam_role_policy_attachment" "ec2-ssm-role" {
+  role = "${aws_iam_role.elasticsearchrole.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
+}
+
