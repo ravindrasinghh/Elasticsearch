94c45d610fadde24fafa79aebbb206e6a0d969b3
ravindrasinghh <rvisingh1221@gmail.com>
2019-12-08 21:48:04 +0530

    update elasticsearch.tf and userdata.sh

--- Elasticsearch.tf
+++ Elasticsearch.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "elasticsearch_instance" {
   ami           = "ami-0927ed83617754711"
+  key_name      = "bastion"
   instance_type = "${var.aws_instance_type}"
   security_groups = ["${aws_security_group.elasticsearch_cluster_sg.name}"]
   iam_instance_profile = "${aws_iam_instance_profile.elasticsearchrole.name}"
-  #user_data = "${file("elasticsearch-node-setup.sh")}"
-  count = "1"
+  user_data = "${file("userdata.sh")}"
 
   tags = {
     Environment = "elk"
--- userdata.sh
+++ userdata.sh
@@ -9,6 +9,9 @@ wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add 
 wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.4.0.tar.gz
 tar -xzvf elasticsearch-5.4.0.tar.gz
 
-curl -u elastic:changeme localhost:9200
+cd /home/ubuntu/elasticsearch-5.4.0/bin
+./elasticsearch-plugin install x-pack
+
+echo curl -u elastic:changeme localhost:9200
 
 
